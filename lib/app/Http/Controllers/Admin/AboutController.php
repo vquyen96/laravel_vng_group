bfb7318308e51c92b834f336b42127f8dc0ca88d
vquyen96 <quyendvd00775@fpt.edu.vn>
2018-05-28 08:15:04 +0700

    loi

--- lib/app/Http/Controllers/Admin/AboutController.php
+++ lib/app/Http/Controllers/Admin/AboutController.php
@@ -36,32 +36,32 @@ class AboutController extends Controller
     	$data['items'] = About::where('name','Lịch sử')->get();
     	return view('backend.about', $data);
     }
-    public function postHistory(Request $request, $id){
-    	$content = "content".$id;
-        $data = About::find($id);
-        $data->title = $request->title;
-        $data->content = $request->$content;
-        $fileimage = $request->file('file');
+    // public function postHistory(Request $request, $id){
+    // 	$content = "content".$id;
+    //     $data = About::find($id);
+    //     $data->title = $request->title;
+    //     $data->content = $request->$content;
+    //     $fileimage = $request->file('file');
         
-        if ($request->file('file') != null) {
-            $filename = time() . '.' .$fileimage->getClientOriginalName();
-            $data->img = $filename;
-            $request->file->storeAs('about',$filename);
-        }
-        $data->save();
-        return back()->with('success','Sửa Thành Công');
-    }
+    //     if ($request->file('file') != null) {
+    //         $filename = time() . '.' .$fileimage->getClientOriginalName();
+    //         $data->img = $filename;
+    //         $request->file->storeAs('about',$filename);
+    //     }
+    //     $data->save();
+    //     return back()->with('success','Sửa Thành Công');
+    // }
     public function getVision(){
     	$data['items'] = About::where('name','Tầm nhìn')->get();
     	return view('backend.about', $data);
     }
-    public function postVision(Request $request){
-    	$data = About::find(4);
-    	$data->name = $request->name;
-    	$data->content = $request->content;
-    	$data->save();
-    	return back()->with('success','Sửa Thành Công');
-    }
+    // public function postVision(Request $request){
+    // 	$data = About::find(4);
+    // 	$data->name = $request->name;
+    // 	$data->content = $request->content;
+    // 	$data->save();
+    // 	return back()->with('success','Sửa Thành Công');
+    // }
     public function getCultural(){
     	$data['items'] =About::where('name','Lịch sử')->get();
     	return view('backend.about', $data);
